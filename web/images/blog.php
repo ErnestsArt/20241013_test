80e1e8abf5db74f9716453991813614fefddf9ff
Ernests <ernests1012@gmail.com>
2024-10-13 18:10:05 +0300

    bloh.php faila papildināšana

--- web/images/blog.php
+++ web/images/blog.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>Tiešsaistes dienasgrāmata</h1>
@@ -13,12 +14,12 @@
     </div>
 
     <div>
-    <h2>Biogrāfija</h2>
+    <h2 class="small-heading">Biogrāfija</h2>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta vitae quae quam culpa quisquam harum mollitia corrupti doloribus facilis officiis! Quo earum sit eveniet omnis. Eaque quo corporis eligendi cupiditate? Consequuntur nulla error commodi expedita! Velit nesciunt tempore quae asperiores omnis minima, accusamus dolor animi soluta mollitia veritatis reiciendis eius labore aut placeat consequuntur in sed repudiandae, nemo non quo eaque! Unde doloremque fuga nobis cupiditate quas architecto quidem enim minima obcaecati? Mollitia nobis ratione, voluptatem nesciunt tenetur distinctio sapiente perferendis quidem pariatur quam unde aliquid obcaecati aut ea laborum quis modi quibusdam laudantium odio aperiam doloremque dolorem temporibus voluptas?</p>
     </div>
 
     <div>
-    <h3>Kāpēc es nolēmu kļūt par programmētāju</h3>
+    <h2 class="small-heading">Kāpēc es nolēmu kļūt par programmētāju</h2>
     </div>
 
     
@@ -29,7 +30,7 @@
         <li>nākotne</li>
         <li>patīk</li>
     </ul>
-    
+
     <script src="styles.css"></script>
 </body>
 </html>
